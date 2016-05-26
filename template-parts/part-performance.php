c931aba4d14b442b54f15c928c89614f6ed3f93c
Joshua Smith <joshsmith01@me.com>
2016-05-25 21:09:07 -0700

    Add a new partial to be called when the front page is loaded. Hopefully future designs will make use of the piece and I won't have to rebuild it.

---
+++ template-parts/part-performance.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshsmith01
+ * Date: 5/25/16
+ * Time: 20:50
+ */
+
+?>
+<section class="performance">
+	<div class="row">
+		<div class="small-12 columns">
+			<h3>“<span class="main-blue">Alqahera</span>” Creative Performance</h3>
+		</div>
+		<div class="row">
+			<div class="small-12 medium-4 columns">1</div>
+			<div class="small-12 medium-4 columns">2</div>
+			<div class="small-12 medium-4 columns">3</div>
+		</div>
+	</div>
+</section>
