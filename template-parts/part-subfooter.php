ee3985b080f3c66ede498f62b6d5b3ed03982c6b
Joshua Smith <joshsmith01@me.com>
2016-06-03 13:56:32 -0700

    Add page part subfooter

---
+++ template-parts/part-subfooter.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshsmith01
+ * Date: 6/3/16
+ * Time: 12:24
+ */
+?>
+
+
+<section class="subfooter">
+
+
+	<div class="row">
+		<div class="small-12 medium-3 columns">
+			<div class="row ">
+				<div class="small-12 medium-2 columns ">
+					<i class="pe-7s-map-marker" aria-hidden="true"></i>
+				</div>
+
+				<div class="small-12 medium-9 columns  ">
+					<h4 class="">Place & Contact</h4>
+					<address>
+						384 Maple Circle
+						Simi Valley, Nevada 47424
+							<hr>
+						(554) 616-9926
+					</address>
+				</div>
+			</div>
+		</div>
+
+		<div class="small-12 medium-3 columns">
+			<div class="row">
+				<div class="small-12 medium-2 columns icon-background">
+					<i class="pe-7s-link" aria-hidden="true"></i>
+				</div>
+				<div class="small-12 medium-9 columns">
+					<h4>Important Links</h4>
+					<ul class="fa-ul">
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#" >Google Maps</a></li>
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">Facebook Social Network</a></li>
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">CNN International</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="small-12 medium-3 columns">
+			<div class="row">
+				<div class="small-12 medium-2 columns icon-background">
+					<i class="pe-7s-link" aria-hidden="true"></i>
+				</div>
+				<div class="small-12 medium-9 columns">
+					<h4>Important Links</h4>
+					<ul class="fa-ul">
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">Google Maps</a></li>
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">Facebook Social Network</a></li>
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">CNN International</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="small-12 medium-3 columns">
+			<div class="row">
+				<div class="small-12 medium-2 columns icon-background">
+					<i class="pe-7s-link" aria-hidden="true"></i>
+				</div>
+				<div class="small-12 medium-9 columns">
+					<h4>Important Links</h4>
+					<ul class="fa-ul">
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">Google Maps</a></li>
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">Facebook Social Network</a></li>
+						<li><i class="fa-li fa fa-chevron-right"></i><a href="#">CNN International</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+
+
+</section>
