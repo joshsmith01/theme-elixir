7ac987afe6265af5d8026026bf57b239f8dd2a86
Joshua Smith <joshsmith01@me.com>
2016-06-03 13:55:56 -0700

    Add page part our-services

---
+++ template-parts/part-our-services.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshsmith01
+ * Date: 6/3/16
+ * Time: 09:46
+ */
+?>
+
+<section class="our-services text-center">
+
+	<div class="row">
+		<div class="small-12 medium-5 columns small-centered our-services-intro">
+			<h3 class="main-blue">Our Services</h3>
+			<p>Professional & Outstanding ideas of our passionate team makes us unique in every sense.</p>
+		</div>
+	</div>
+
+</section>
+
+
+
+<section class="grid-box-bg">
+	<div class="row text-center">
+		<div class="small-12 medium-4 columns">
+			<div class="small-12 service-card">
+				<i class="pe-7s-micro"></i>
+				<h4>Voice Systems</h4>
+			</div>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<div class="small-12 service-card">
+				<i class="pe-7s-config"></i>
+				<h4>Control Panel</h4>
+			</div>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<div class="small-12 service-card">
+				<i class="pe-7s-bell"></i>
+				<h4>Reminder</h4>
+			</div>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<div class="small-12 service-card">
+				<i class="pe-7s-joy"></i>
+				<h4>Entertainment</h4>
+			</div>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<div class="small-12 service-card">
+				<i class="pe-7s-graph1"></i>
+				<h4>Project Management</h4>
+			</div>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<div class="small-12 service-card">
+				<i class="pe-7s-alarm"></i>
+				<h4>Watching Clock</h4>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="our-services-cta text-center">
+
+<div class="row">
+	<div class="small-12 medium-5 small-centered text-center">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		<div class="button">Get Your Place</div>
+	</div>
+</div>
+
+
+</section>
