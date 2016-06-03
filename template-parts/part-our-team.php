004f1bb9a9af56974db40cc2ab1e3b32797bbd36
Joshua Smith <joshsmith01@me.com>
2016-06-03 13:56:13 -0700

    Add page part our-team

---
+++ template-parts/part-our-team.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshsmith01
+ * Date: 6/1/16
+ * Time: 12:46
+ */
+?>
+
+<section class="our-team">
+	<div class="row section-intro expanded">
+		<div class="small-12 medium-6 medium-centered columns text-center">
+			<h3>Our Team</h3>
+			<p>Professional & Outstanding ideas of our passionate team makes us unique in every sense.</p>
+		</div>
+	</div>
+
+	<div class="row meet-the-team text-center">
+		<div class="small-12 columns">
+			<h4>Meet the Team</h4>
+			<p>Nunc laoreet sapien sed turpis tempus consequat Aenean ullamcorper risus vel urna tristique, ut fringilla nulla rutrum.</p>
+		</div>
+	</div>
+
+	<div class="row text-center team-images">
+		<div class="small-4 columns">
+			<div class="small-12 column teammate row">
+				<img src="http://placehold.it/370x370" alt="">
+				<div class="teammate-contact">
+					<h5>Brittany Hudson</h5>
+					<p class="team-job-title">Model</p>
+					<div class="align-center social-icon-container">
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-facebook teammate-social" aria-hidden="true"></i></a>
+							</span>
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-twitter teammate-social" aria-hidden="true"></i></a>
+							</span>
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-pinterest teammate-social" aria-hidden="true"></i></a>
+							</span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="small-4 columns">
+			<div class="small-12 column teammate row">
+				<img src="http://placehold.it/370x370" alt="">
+				<div class="teammate-contact">
+					<h5>Brittany Hudson</h5>
+					<p class="team-job-title">Model</p>
+					<div class="align-center social-icon-container">
+							<span class="flex-child">
+								<a href="#" ><i class="fa fa-facebook teammate-social" aria-hidden="true"></i></a>
+							</span>
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-twitter teammate-social" aria-hidden="true"></i></a>
+							</span>
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-pinterest teammate-social" aria-hidden="true"></i></a>
+							</span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="small-4 columns">
+			<div class="small-12 column teammate row">
+				<img src="http://placehold.it/370x370" alt="">
+				<div class="teammate-contact">
+					<h5>Brittany Hudson</h5>
+					<p class="team-job-title">Model</p>
+					<div class="align-center social-icon-container">
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-facebook teammate-social" aria-hidden="true"></i></a>
+							</span>
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-twitter teammate-social" aria-hidden="true"></i></a>
+							</span>
+							<span class="flex-child">
+								<a href="#"><i class="fa fa-pinterest teammate-social" aria-hidden="true"></i></a>
+							</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
